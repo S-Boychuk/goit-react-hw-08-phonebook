c6b5772a6b70c9fa7d3609607ce76a1177671616
S-Boychuk <112415143+S-Boychuk@users.noreply.github.com>
2023-07-13 13:44:50 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import Section from './Section/Section';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+const App = () => {
+  return (
+    <>
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        <ContactList />
+      </Section>
+    </>
+  );
+};
+
+export default App;
